05200b3a105bb2916f2988518c25eddac5ace3f9
구자건 <wkrjsdl5632@gmail.com>
2024-06-17 16:22:53 +0900

    feat: component 학습

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(props){
+
+function Header(props) {
   console.log('props', props);
   return (
     <header>
-      <h1><a href='/'>{props.title}</a></h1>
+      <h1><a href="/">{props.title}</a></h1>
     </header>
-  )
+  );
 }
-function Nav(){
-  return(
+
+function Nav(props) {
+  const lis = props.topics.map(t => (
+    <li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>
+  ));
+  return (
     <nav>
       <ol>
-        <li><a href = "/read/1">html</a></li>
-        <li><a href = "/read/2">css</a></li>
-        <li><a href = "/read/3">js</a></li>
+        {lis}
       </ol>
     </nav>
-  )
+  );
 }
-function Article(props){
-  return(
+
+function Article(props) {
+  return (
     <article>
       <h2>{props.title}</h2>
-      <body>{props.body}</body>
+      <div>{props.body}</div>
     </article>
-  )
+  );
 }
+
 function App() {
+  const topics = [
+    {id: 0, title: 'html', body: 'html is ...'},
+    {id: 1, title: 'css', body: 'css is ...'},
+    {id: 2, title: 'javascript', body: 'javascript is ...'}
+  ];
   return (
     <div>
-      <Header title = "안녕 친구야"></Header>
-      <Nav></Nav>
-      <Article title = "Hi" body = "Hello WEB"></Article>
+      <Header title="안녕 친구야" />
+      <Nav topics={topics} />
+      <Article title="Hi" body="Hello WEB" />
     </div>
   );
 }
